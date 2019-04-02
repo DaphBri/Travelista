e0e75009c72dd354acf77cfa6c7281db72e165fc
jodedeck <jodedeck542@gmail.com>
2019-04-02 13:19:07 +0200

    vue-deuxieme

--- src/js/vue.js
+++ src/js/vue.js
@@ -1,15 +1,46 @@
 new Vue({
     el: "#main",
+    
     data() {
         return{
-            hollys:[]
+            hollys:[],
+            france:[],
+            spain:[],
+            belgium:[],
+            hideValue:true,
+            popValue:true
         }
     },
+    methods:{
+        showBlock(){
+            this.hideValue = false
+        },
+        showSel(){
+            this.popValue = false
+        },
+        filter(type) {    
+            
+            if(type==='France'){
+                this.hollys = this.france; 
+            }        
+            else if(type === 'Spain'){
+                this.hollys = this.spain;
+            }
+            else if(type === 'Belgium'){
+                this.hollys = this.belgium;
+            }                                  
+        }
+
+    },
+
     mounted() {
         axios.get("https://wt-4abc83e5c2056740a9e00a6e0975a49a-0.sandbox.auth0-extend.com/city-trip")
-            .then(response => {
-                this.hollys = response.data;                //response data in axios, under #main!
-            })
+        .then(response => {
+            this.hollys = response.data;         //response data in axios, under #main!
+            this.france = this.hollys.filter(t => t.countryName ==='France');       //select list France'
+            this.spain = this.hollys.filter(t => t.countryName ==='Spain');       //select list Spain'
+            this.belgium = this.hollys.filter(t => t.countryName ==='Belgium');       //select list Belgium'
+        })
     }
 })
 
